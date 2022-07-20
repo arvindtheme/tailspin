39e151fd65871b8ec39cd58324a7fce426d1bf9a
Bezil M <95276660+Bezil-Bennet@users.noreply.github.com>
2022-07-20 11:04:19 +0530

    Add files via upload

---
+++ Simple/src/main/java/google/TC001.java
@@ -0,0 +1,18 @@
+package google;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC001 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		String title = driver.getTitle();
+		System.out.println("Title - "+title);
+		Assert.assertEquals("Google", title);
+		System.out.println("testing done");
+	}
+}
